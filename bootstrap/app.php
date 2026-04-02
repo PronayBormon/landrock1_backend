43770e08a99ef4e254b1a3c5fc4634fda8e1f7ca
pronaybormon <pronaykumarbormon@gmail.com>
2026-04-02 12:21:38 +0600

    update localization language

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,11 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'admin' => AdminMiddleware::class,
+            'localize' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRoutes::class,
+            'localizationRedirect' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationRedirectFilter::class,
+            'localeSessionRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleSessionRedirect::class,
+            'localeCookieRedirect' => \Mcamara\LaravelLocalization\Middleware\LocaleCookieRedirect::class,
+            'localeViewPath' => \Mcamara\LaravelLocalization\Middleware\LaravelLocalizationViewPath::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
---
+++ resources/lang/en/menu.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'users' => 'Users',
+    'faq' => 'FAQ',
+    'pages' => 'Pages',
+    'more' => 'More',
+    'settings' => 'Settings',
+    'system_setting' => 'System Settings',
+    'smtp_setting' => 'SMTP Settings',
+    'stripe_setting' => 'Stripe Settings',
+    'paypal_setting' => 'Paypal Settings',
+];
